6def1751caafea5192d107e9520acf4c07249b4a
siddharth <zeddharth@gmail.com>
2021-12-21 14:00:11 +0530

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import Head from "next/head";
 import classes from "../styles/Home.module.css";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import useCandyMachine from "../hooks/useCandyMachine";
-import useWalletBalance from "../hooks/useWalletBalance";
-import Slider from '@mui/material/Slider';
-import { Toaster } from "react-hot-toast";
-import useWalletNfts from "../hooks/useWalletNFTs";
-import Box from '@mui/material/Box';
-import AnNFT from "../components/AnNFT/AnNFT";
 
 export default function Home() {
-  const [balance] = useWalletBalance();
-  const {
-    isSoldOut,
-    mintStartDate,
-    isMinting,
-    startMint,
-    startMintMultiple,
-    nftsData,
-  } = useCandyMachine();
-
-  const [isLoading, nfts] = useWalletNfts();
-
-
-
-
-
-  const MintMany = () => {
-   
-    return (
-      <div className={classes.btnvalidiv}>
-       
-        
-       
-      </div>
-    );
-  };
-
   return (
     <>
       <Head>
